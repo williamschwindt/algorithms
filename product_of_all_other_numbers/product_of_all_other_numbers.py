a79919ec6296c8396c2c424836c2b3163fcccd5a
William Schwindt <williammschwindt@gmail.com>
2020-07-16 13:31:11 -0700

    first 3 optimized

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -2,21 +2,33 @@
 Input: a List of integers
 Returns: a List of integers
 '''
-def product_of_all_other_numbers(arr):
-    #keep track of the products
-    new_arr = []
-    #loop through array
-    for i in range(0, len(arr)):
-        #reset the product after every outside loop iteration
-        product = -1
-        #loop through again
-        for j in range(0, len(arr)):
-            #calculate product
-            product = product * arr[j] 
-        #divide the total product by arr[i] and * -1 
-        new_arr.append((product * -1) / arr[i])
+# def product_of_all_other_numbers(arr):
+#     #keep track of the products
+#     new_arr = []
+#     #loop through array
+#     for i in range(0, len(arr)):
+#         #reset the product after every outside loop iteration
+#         product = 1
+#         #loop through again
+#         for j in range(0, len(arr)):
+#             #calculate product
+#             product = product * arr[j] 
+#         #divide the total product by arr[i]
+#         new_arr.append(product / arr[i])
 
-    return new_arr
+#     return new_arr
+
+def product_of_all_other_numbers(arr):
+    returned_arr = [0 for num in arr]
+    product = 1
+
+    for num in arr:
+        product = (product * num)
+    
+    for i in range(0, len(arr)):
+        returned_arr[i] = product / arr[i]
+
+    return returned_arr
 
 arr = [1, 2, 3, 4, 5]
 print(product_of_all_other_numbers(arr))
--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,18 +2,36 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
+# O(n^2)
+# def single_number(arr):
+#     #loop over the array on keep track of the current int
+#     for i in range(0, len(arr)):
+#     #loop over again and compare that int to every other int
+#         for j in range(0, len(arr)):
+#     #if there is a duplicate pop off both ints
+#             if arr[j] == arr[i] and i != j:
+#                 arr[j] = ''
+#                 arr[i] = ''
+#     for i in arr:
+#         if i != '':
+#             return i
+
+
+#O(n)
 def single_number(arr):
-    #loop over the array on keep track of the current int
-    for i in range(0, len(arr)):
-    #loop over again and compare that int to every other int
-        for j in range(0, len(arr)):
-    #if there is a duplicate pop off both ints
-            if arr[j] == arr[i] and i != j:
-                arr[j] = ''
-                arr[i] = ''
-    for i in arr:
-        if i != '':
-            return i
+    #create a cache
+    count_cache = {}
+    #loop over and insert the count of each number into the cache
+    for num in arr:
+        if num in count_cache:
+            count_cache[num] += 1
+        else:
+            count_cache[num] = 1
+    #loop over and return the number that has a count of 1
+    for num in arr:
+        if count_cache[num] == 1:
+            return num
+
 
 
 if __name__ == '__main__':
