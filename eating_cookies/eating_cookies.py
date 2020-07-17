1dfa7821c15ae01a13975ed8aa19a5a00d5a6da7
William Schwindt <williammschwindt@gmail.com>
2020-07-16 17:42:43 -0700

    sliding window and eating cookies optimized

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,21 +2,19 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    count = 0
+def eating_cookies(n, cache={0:1,1:1,2:2}):
 
-    if n <= 1:
-        return 1
-    elif n == 2:
-        return 2
-    elif n == 3:
-        return 4
+    if n in cache:
+        return cache[n]
+    
     else:
-        return eating_cookies(n - 3) + eating_cookies(n - 2) + eating_cookies(n - 1)
-    return count
+        cache[n] = eating_cookies(n - 3) + eating_cookies(n - 2) + eating_cookies(n - 1)
+        return cache[n]
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 5
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    print(f"There are {eating_cookies(num_cookies, {})} ways for Cookie Monster to each {num_cookies} cookies")
+
+    
